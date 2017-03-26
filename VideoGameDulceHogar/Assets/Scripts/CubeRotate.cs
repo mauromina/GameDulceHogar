ee9ae3148e30821e324b855799873d8c6eb5d01b
mauroctm <mauroctm7@gmail.com>
2017-03-26 13:55:39 -0500

    Add scene park and urban
    Creacion de escenarios de parque y urbano para el juego dulce hogar.
    Sonido y personajes

---
+++ VideoGameDulceHogar/Assets/Scripts/CubeRotate.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeRotate : MonoBehaviour {
+
+	// Use this for initialization
+	public float speed = 2;
+	public GameObject ObjetoGame;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ObjetoGame.transform.Rotate (new Vector3(1,45,1)* Time.deltaTime * speed);
+	}
+}
